31f1de8b970461f53ef102477475da2849a94de2
macjankowski <mac.jankowski@gmail.com>
2013-10-13 22:28:47 +0200

    added implementation of sequence for Option

---
+++ src/main/scala/pl/macjankowski/fps/ch4/OptionMethods.scala
@@ -0,0 +1,18 @@
+package pl.macjankowski.fps.ch4
+
+/**
+ * @author "Maciej Jankowski <mac.rarry@gmail.com>"
+ *
+ */
+object OptionMethods {
+
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = a.foldRight(Option(List[A]()))((e, acc) => e match {
+    case None => None
+    case Some(a) => acc.fold(None: Option[List[A]])(l => Some(a :: l))
+  }).fold(None: Option[List[A]])(_ match {
+    case List() => None
+    case x => Option(x)
+  })
+
+
+}
---
+++ src/test/scala/pl/macjankowski/fps/ch4/TestOptionMethods.scala
@@ -0,0 +1,29 @@
+package pl.macjankowski.fps.ch4
+
+import org.scalatest.FunSpec
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.prop.TableDrivenPropertyChecks
+
+/**
+ * @author "Maciej Jankowski <mac.rarry@gmail.com>"
+ *
+ */
+class TestOptionMethods extends FunSpec with ShouldMatchers with TableDrivenPropertyChecks {
+
+  val tests = Table(
+    ("list", "res"),
+    (List(), None),
+    (List(Some(1), Some(3), Some(5)), Some(List(1, 3, 5))),
+    (List(Some(1), None, Some(5)), None),
+    (List(Some(1)), Some(List(1))),
+    (List(None, Some(3), Some(5)), None),
+    (List(Some(1), Some(3), Some(5), None), None))
+
+  describe("Option.sequence") {
+    forAll(tests) { (list, res) =>
+      it(s"list $list should return $res") {
+        OptionMethods.sequence(list) should equal(res)
+      }
+    }
+  }
+}
