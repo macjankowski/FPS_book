d4dc5ff2177cd0e351523afe7cc088bad2f71d5c
jankowski <maciej.jankowski@ser-solutions.pl>
2014-08-14 13:29:37 +0200

    added excersise 5

---
+++ src/main/scala/pl/macjankowski/fps/ch4/Ex5.scala
@@ -0,0 +1,15 @@
+package pl.macjankowski.fps.ch4
+
+/**
+ * Created with IntelliJ IDEA.
+ * @author Maciej Jankowski <maciej.jankowski@ser-solutions.pl>
+ *         Date: 14.08.14
+ *         Time: 13:16
+ */
+class Ex5 {
+
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
+    case Nil =>  None
+    case x::xs => x.flatMap(e => sequence(xs) map(l => e :: l))
+  }
+}
