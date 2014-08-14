3a52837bf0af59822ff2a53ab89e9a18701d0d4f
jankowski <maciej.jankowski@ser-solutions.pl>
2014-08-14 13:53:13 +0200

    excersise 6

--- src/main/scala/pl/macjankowski/fps/ch4/Ex5.scala
+++ src/main/scala/pl/macjankowski/fps/ch4/Ex5.scala
@@ -10,6 +10,15 @@ class Ex5 {
 
   def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
     case Nil =>  None
-    case x::xs => x.flatMap(e => sequence(xs) map(l => e :: l))
+    case x::xs => x flatMap(e => sequence(xs) map(l => e :: l))
   }
+}
+
+class Ex6 {
+
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = a match {
+    case Nil =>  None
+    case x::xs => f(x).flatMap(e => traverse(xs)(f) map(l => e :: l))
+  }
+
 }
